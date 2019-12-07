52420035c2f36d229ab1caa03a8a9db7b0948dec
Naomi <naomi.paulmin@gmail.com>
2019-12-07 21:17:25 +0100

    CRUD clients

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const ClientController = require('../controllers/client')
+const Joi = require('joi')
+
 const Client = {
   name: 'Client',
   version: '1.0.0',
@@ -13,6 +15,90 @@ const Client = {
         config: { auth: false },
         handler: async (req, h) => {
           return await ClientController.getAllClients(req, h)
+        }
+      },
+      {
+        method: 'GET',
+        path: '/clients/{id}',
+        config: {
+          auth: false,
+          validate: {
+            params: {
+              id: Joi.string()
+                .max(11)
+                .required()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await ClientController.getClient(req, h)
+        }
+      },
+      {
+        method: 'POST',
+        path: '/clients',
+        config: {
+          auth: false,
+          validate: {
+            payload: {
+              nom: Joi.string().required(),
+              prenom: Joi.string().required(),
+              adresse: Joi.string().optional(),
+              date_naissance: Joi.string().optional(),
+              civilite: Joi.string().optional(),
+              numero: Joi.string().required(),
+              id_ville: Joi.number().optional()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await ClientController.createClient(req, h)
+        }
+      },
+      {
+        method: 'PUT',
+        path: '/clients/{id}',
+        config: {
+          auth: false,
+          validate: {
+            params: {
+              id: Joi.string()
+                .max(11)
+                .required()
+            },
+            payload: {
+              id_client: Joi.string()
+                .max(11)
+                .required(),
+              nom: Joi.string().required(),
+              prenom: Joi.string().required(),
+              adresse: Joi.string().optional(),
+              date_naissance: Joi.string().optional(),
+              civilite: Joi.string().optional(),
+              numero: Joi.string().required(),
+              id_ville: Joi.number().optional()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await ClientController.updateClient(req, h)
+        }
+      },
+      {
+        method: 'DELETE',
+        path: '/clients/{id}',
+        config: {
+          auth: false,
+          validate: {
+            params: {
+              id: Joi.string()
+                .max(11)
+                .required()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await ClientController.deleteClient(req, h)
         }
       }
     ])
