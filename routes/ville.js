0a52dc5f63e10f1a0b16e2d20d21cd3cf7cae8a7
Naomi <naomi.paulmin@gmail.com>
2019-12-12 15:57:21 +0100

    CRUD ville

--- routes/ville.js
+++ routes/ville.js
@@ -18,6 +18,43 @@ const Ville = {
         }
       },
       {
+        method: 'POST',
+        path: '/villes',
+        config: {
+          auth: false,
+          validate: {
+            payload: {
+              nom: Joi.string().required(),
+              code_postal: Joi.string().required(),
+              code_pays: Joi.string()
+                .max(2)
+                .required(),
+              id_maire: Joi.string().optional()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await VilleController.createCity(req, h)
+        }
+      },
+      {
+        method: 'GET',
+        path: '/villes/zipcode/{zipcode}',
+        config: {
+          auth: false,
+          validate: {
+            params: {
+              zipcode: Joi.string()
+                .max(10)
+                .required()
+            }
+          }
+        },
+        handler: async (req, h) => {
+          return await VilleController.getCityByZipcode(req, h)
+        }
+      },
+      {
         method: 'GET',
         path: '/villes/{id}',
         config: {
@@ -31,7 +68,7 @@ const Ville = {
           }
         },
         handler: async (req, h) => {
-          return await VilleController.getCity(req, h)
+          return await VilleController.getCityById(req, h)
         }
       }
     ])
