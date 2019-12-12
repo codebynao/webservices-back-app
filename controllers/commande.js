8f8927266a48ebfe32224a6d93d6b2ef608e8f74
Naomi <naomi.paulmin@gmail.com>
2019-12-12 17:28:04 +0100

    feat: get all orders and add client to order

--- controllers/commande.js
+++ controllers/commande.js
@@ -1,12 +1,35 @@
 'use strict'
 
 const Models = require('../models')
+const ClientModel = Models.client
 const CommandeModel = Models.commande
 const CommandeProduitModel = Models.commande_produit
+const VilleModel = Models.ville
 const ProduitModel = Models.produit
 const lib = require('../lib')
 
 class Ville {
+  async getOrders(req, h) {
+    try {
+      const orders = await CommandeModel.findAll({ raw: true })
+      for (const order of orders) {
+        const products = await CommandeProduitModel.findAll({
+          where: { id_commande: order.id_commande },
+          raw: true
+        })
+
+        // Add total order price
+        order.nombre_produits = products.length
+        order.total = lib.getOrderTotalPrice(products)
+      }
+      return {
+        code: 200,
+        data: orders
+      }
+    } catch (error) {
+      return lib.formatErrorResponse(500, error)
+    }
+  }
   async getUserOrders(req, h) {
     try {
       const orders = await CommandeModel.findAll({
@@ -74,6 +97,40 @@ class Ville {
 
       // Get order total price
       order.total = lib.getOrderTotalPrice(orderProducts)
+
+      // Get client
+      const client = await ClientModel.findOne({
+        where: { id_client: order.id_client },
+        raw: true
+      })
+
+      // If no client found, return empty client
+      if (!client) {
+        order.client = null
+        return {
+          code: 200,
+          data: order
+        }
+      }
+
+      // Get city from client's city
+      const ville = await VilleModel.findOne({
+        where: { id_ville: client.id_ville },
+        raw: true
+      })
+
+      // Format ville with street name, zip code, city and country code
+      if (ville) {
+        const rue = client.adresse
+        client.adresse = {
+          rue,
+          code_postal: ville.code_postal,
+          ville: ville.nom,
+          code_pays: ville.code_pays
+        }
+      }
+
+      order.client = client
 
       return {
         code: 200,
