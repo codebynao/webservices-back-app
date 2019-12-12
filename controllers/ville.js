41e228a7dfc1211cdf155c39b1d56a34cb859f8d
Naomi <naomi.paulmin@gmail.com>
2019-12-12 15:57:21 +0100

    CRUD ville

--- controllers/ville.js
+++ controllers/ville.js
@@ -18,10 +18,12 @@ class Ville {
     }
   }
 
-  async getCity(req, h) {
+  async getCityById(req, h) {
     try {
       const city = await VilleModel.findOne({
-        where: { id_ville: req.params.id },
+        where: {
+          id_ville: req.params.id
+        },
         raw: true
       })
 
@@ -36,6 +38,65 @@ class Ville {
       return {
         code: 200,
         data: city
+      }
+    } catch (error) {
+      return lib.formatErrorResponse(500, error)
+    }
+  }
+
+  async getCityByZipcode(req, h) {
+    try {
+      const city = await VilleModel.findOne({
+        where: {
+          code_postal: req.params.zipcode
+        },
+        raw: true
+      })
+
+      if (!city) {
+        return lib.formatErrorResponse(
+          404,
+          `La ville ayant le code postal ${req.params.zipcode} est introuvable`,
+          true
+        )
+      }
+
+      return {
+        code: 200,
+        data: city
+      }
+    } catch (error) {
+      return lib.formatErrorResponse(500, error)
+    }
+  }
+
+  async createCity(req, h) {
+    try {
+      const city = await VilleModel.findOne({
+        where: { code_postal: req.payload.code_postal }
+      })
+
+      if (city) {
+        return lib.formatErrorResponse(
+          404,
+          `La ville ayant le code postal ${req.params.zipcode} existe déjà`,
+          true
+        )
+      }
+
+      const id_ville = await lib.getLastId(VilleModel, 'id_ville')
+
+      const createdCity = await VilleModel.create({
+        id_ville,
+        nom: req.payload.nom,
+        code_postal: req.payload.code_postal,
+        code_pays: req.payload.code_pays,
+        ...(req.payload.id_maire && { id_maire: req.payload.id_maire })
+      })
+
+      return {
+        code: 201,
+        data: createdCity
       }
     } catch (error) {
       return lib.formatErrorResponse(500, error)
